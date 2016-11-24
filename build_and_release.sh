3a6c05e823dd3acd33b9d96e61f61313b48f2271
Paul Thomson <paul.thomson11@imperial.ac.uk>
2016-11-24 16:47:11 +0000

    Updated ci to also build desktop version.

--- build_and_release.sh
+++ build_and_release.sh
@@ -3,12 +3,16 @@ set -x
 set -e
 set -u
 
+rm -rf build_release || true
+mkdir build_release
+./gradlew desktop:dist
+cp desktop/build/libs/desktop-1.0.jar build_release/libgdx-get-image-desktop.jar
 ./gradlew android:assembleDebug
-cd android/build/outputs/apk/
+cp android/build/outputs/apk/android-debug.apk build_release/libgdx-get-image-android-debug.apk
 github-release \
   mc-imperial/libgdx-get-image \
   v-${CI_BUILD_REF} \
   ${CI_BUILD_REF} \
   "$(echo -e "Automated build.\n$(git log --graph -n 3 --abbrev-commit --pretty='format:%h - %s <%an>')")" \
-  android-debug.apk
+  'build_release/*'
 
